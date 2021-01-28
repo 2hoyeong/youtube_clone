8d473ff4c3949f0bb5bcbd591c9b63758931c582
2hoyeong <leehodud302@naver.com>
2021-01-28 16:25:05 +0900

    [be] feat : UserRepository 테스트 구현
    findById 함수 테스트 구현

--- src/test/java/me/hoyoung/youtube/domain/user/UserRepositoryTest.java
+++ src/test/java/me/hoyoung/youtube/domain/user/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package me.hoyoung.youtube.domain.user;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,7 @@ public class UserRepositoryTest {
 
     @Test
     @Transactional
+    @DisplayName("유저 save 테스트")
     public void userSignUpTest() throws Exception {
         //given
         String id = "testId";
@@ -44,5 +46,28 @@ public class UserRepositoryTest {
         assertThat(user.getId()).isEqualTo(id);
         assertThat(user.getPassword()).isEqualTo(password);
         assertThat(user.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("유저 findById 테스트")
+    public void userSignInTest() throws Exception {
+        //given
+        String id = "testId";
+        String password = "testPassword";
+        String name = "테스트용 계정";
+
+        userRepository.save(User.builder()
+        .id(id)
+        .password(password)
+        .name(name).build());
+
+        //when
+        User user = userRepository.findById(id).orElseThrow(() -> new Exception("사용자를 찾을 수 없습니다."));
+
+        //then
+        assertThat(user.getId()).isEqualTo(id);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getPassword()).isEqualTo(password);
     }
 }
