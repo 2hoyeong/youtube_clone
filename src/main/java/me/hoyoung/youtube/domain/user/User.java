eb8d3a45824e834478ef11f42ef5ad7e76ea4bab
2hoyeong <leehodud302@naver.com>
2021-02-02 00:39:24 +0900

    [be] feat : User 엔티티 수정
    User 엔티티에 권한이 저장되도록 수정하고 UserDetails 인터페이스를 구현

--- src/main/java/me/hoyoung/youtube/domain/user/User.java
+++ src/main/java/me/hoyoung/youtube/domain/user/User.java
@@ -3,12 +3,18 @@ package me.hoyoung.youtube.domain.user;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,10 +29,45 @@ public class User {
     @Column(length = 20, nullable = false)
     private String password;
 
+    @Column
+    private String role = Role.USER.getKey();
+
     @Builder
     public User(String id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+        auth.add(new SimpleGrantedAuthority(this.role));
+        return auth;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
