e3b41fef8cf0cf4e4c93ddb75a3d50520e4f3d88
2hoyeong <leehodud302@naver.com>
2021-02-04 01:32:33 +0900

    [be] feat : VideoController 구현
    upload Controller 구현

---
+++ src/main/java/me/hoyoung/youtube/web/api/VideoController.java
@@ -0,0 +1,34 @@
+package me.hoyoung.youtube.web.api;
+
+import lombok.RequiredArgsConstructor;
+import me.hoyoung.youtube.domain.video.Video;
+import me.hoyoung.youtube.service.VideoService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/video")
+public class VideoController {
+
+    private final VideoService videoService;
+
+    @PostMapping(
+            value = "/upload",
+            consumes = {
+                    MediaType.MULTIPART_FORM_DATA_VALUE,
+                    MediaType.APPLICATION_OCTET_STREAM_VALUE})
+    public ResponseEntity<?> createVideo(
+            @RequestPart("content") MultipartFile file) throws IOException {
+        String contentType = file.getContentType();
+
+        Video metaData = videoService.createFile(file);
+        return ok(metaData);
+    }
+}
