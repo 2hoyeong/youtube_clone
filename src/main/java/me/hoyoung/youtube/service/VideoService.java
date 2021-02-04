6e21f8a06ce2227691d94052d0e1799251af52f0
2hoyeong <leehodud302@naver.com>
2021-02-04 23:56:42 +0900

    [be] feat : VideoDrive Service 연결
    Videodrive 서비스와 연결해서 비디오를 드라이브에 저장하고, DB에도 저장하도록 로직을 구현

--- src/main/java/me/hoyoung/youtube/service/VideoService.java
+++ src/main/java/me/hoyoung/youtube/service/VideoService.java
@@ -2,10 +2,12 @@ package me.hoyoung.youtube.service;
 
 import lombok.RequiredArgsConstructor;
 import me.hoyoung.youtube.domain.video.Video;
+import me.hoyoung.youtube.domain.video.VideoDrive;
 import me.hoyoung.youtube.domain.video.VideoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
@@ -13,12 +15,17 @@ import java.time.LocalDateTime;
 @Service
 public class VideoService {
     private final VideoRepository videoRepository;
+    private final VideoDrive videoDrive;
 
-    public Video createFile(MultipartFile file) {
+    public Video createFile(MultipartFile file) throws IOException {
+        String filename = videoDrive.createFile(file);
+
         Video video = Video.builder()
-                .originalFileName(file.getName().toString())
+                .originalFileName(file.getName())
                 .createdDate(Timestamp.valueOf(LocalDateTime.now()))
+                .originalFileName(filename)
                 .build();
+
 
         return videoRepository.save(video);
     }
