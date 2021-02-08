d33298726f198e502bde57adc20a35dde8828714
2hoyeong <leehodud302@naver.com>
2021-02-09 00:52:54 +0900

    [be] feat : VideoStream 컨트롤러 구현
    저장된 Video를 불러와 Stream을 응답해 주는 컨트롤러 구현

--- src/main/java/me/hoyoung/youtube/web/api/VideoController.java
+++ src/main/java/me/hoyoung/youtube/web/api/VideoController.java
@@ -6,8 +6,9 @@ import me.hoyoung.youtube.service.VideoService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.io.IOException;
+import java.io.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,5 +26,35 @@ public class VideoController {
             @RequestPart("content") MultipartFile file) throws IOException {
         String contentType = file.getContentType();
         Video metaData = videoService.createFile(file);
+    }
+
+    @GetMapping(value = "/view/{id}", produces = "video/mp4")
+    public StreamingResponseBody getVideoStream(@PathVariable("id") String id) throws IOException {
+        Video video = videoService.findById(id);
+        File file = videoService.getFile(video.getOriginalFileName());
+        final InputStream is = new FileInputStream(file);
+
+        return os -> {
+            readAndWrite(is, os);
+        };
+    }
+
+    private void readAndWrite(final InputStream is, OutputStream os) {
+        try {
+            byte[] data = new byte[4096];
+            int read = 0;
+            while ((read = is.read(data)) > 0) {
+                os.write(data, 0, read);
+                os.flush();
+            }
+        } catch(IOException ex) {
+
+        } finally {
+            try {
+                is.close();
+                os.close();
+            } catch(IOException ex) {
+            }
+        }
     }
 }
