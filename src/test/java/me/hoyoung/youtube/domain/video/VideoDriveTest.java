98979f424f7983e399431c8a16ef267eccef5fc3
2hoyeong <leehodud302@naver.com>
2021-02-15 23:08:12 +0900

    [be] test : VideoDrive createFile 테스트 구현
    createFile 함수에 대한 테스트를 구현

---
+++ src/test/java/me/hoyoung/youtube/domain/video/VideoDriveTest.java
@@ -0,0 +1,60 @@
+package me.hoyoung.youtube.domain.video;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class VideoDriveTest {
+
+    @Autowired
+    private VideoDrive videoDrive;
+
+    @Value("${video.storage.dir}")
+    private String directory;
+
+    @BeforeEach
+    public void createDir() {
+        File dir = new File(directory);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+    }
+
+    @Test
+    @DisplayName("비디오 createFile 테스트")
+    public void userSignUpTest() throws Exception {
+        //given
+        String filename = UUID.randomUUID().toString() + ".tests";
+        byte[] contents = "Test contents".getBytes();
+        Path path = Paths.get(directory + filename);
+        Files.write(path, contents);
+        MultipartFile multipartFile = new MockMultipartFile(filename, filename, "text/plain",  new FileInputStream(new File(directory + filename)));
+
+        //when
+        String createdFileName = videoDrive.createFile(multipartFile);
+
+        //then
+        File createFile = new File(directory + createdFileName);
+        assertThat(createFile.exists()).isTrue();
+    }
+}
