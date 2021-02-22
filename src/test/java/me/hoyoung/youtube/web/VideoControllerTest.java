81bf0f09e8469e8beb36d9f166806b358e133024
2hoyeong <leehodud302@naver.com>
2021-02-23 00:37:13 +0900

    [be] feat : video 스트림 컨트롤러 테스트 구현
    저장된 video의 스트림을 정상적으로 받아 오는지 테스트 하는 테스트 작성

--- src/test/java/me/hoyoung/youtube/web/VideoControllerTest.java
+++ src/test/java/me/hoyoung/youtube/web/VideoControllerTest.java
@@ -2,6 +2,7 @@ package me.hoyoung.youtube.web;
 
 import me.hoyoung.youtube.domain.user.User;
 import me.hoyoung.youtube.domain.user.UserRepository;
+import me.hoyoung.youtube.domain.video.Video;
 import me.hoyoung.youtube.domain.video.VideoRepository;
 import me.hoyoung.youtube.util.MockFile;
 import org.junit.jupiter.api.AfterEach;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -21,8 +23,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -40,6 +43,9 @@ public class VideoControllerTest {
 
     @Autowired
     private MockFile mockFile;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     private MockMvc mvc;
     private UserDetails userDetails;
@@ -71,5 +77,24 @@ public class VideoControllerTest {
                 .file("content", mockVideo.getBytes())
                 .with(user(userDetails)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void videoStreamTest() throws Exception {
+        String innerText = "videoStreamAPI TEST";
+        MultipartFile mockVideo = mockFile.createRandomFile(innerText);
+        mvc.perform(MockMvcRequestBuilders.multipart("/api/v1/video/upload")
+                .file("content", mockVideo.getBytes())
+                .with(user(userDetails)))
+                .andExpect(status().isOk());
+
+        Video video = videoRepository.findAll().get(0);
+        String videoId = video.getId();
+
+        mvc.perform(get("/api/v1/video/view/" + videoId))
+                .andExpect(request().asyncStarted())
+                .andExpect(status().isOk())
+                .andExpect(content().string(innerText));
     }
 }
