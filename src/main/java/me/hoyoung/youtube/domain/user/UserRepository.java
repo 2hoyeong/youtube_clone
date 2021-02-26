9899aa1bd4155ccee90d8bf43e75da4a85cb5eec
2hoyeong <leehodud302@naver.com>
2021-02-26 23:55:45 +0900

    [be] feat : 유저 id로 비디오 리스트 반환하는 함수 구현
    유저 ID로 업로드된 비디오 리스트를 반환하는 쿼리 작성하여 함수 구현

--- src/main/java/me/hoyoung/youtube/domain/user/UserRepository.java
+++ src/main/java/me/hoyoung/youtube/domain/user/UserRepository.java
@@ -1,11 +1,17 @@
 package me.hoyoung.youtube.domain.user;
 
+import me.hoyoung.youtube.domain.video.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(String id);
+
+    @Query("SELECT id, thumbnailPath, createdDate FROM Video WHERE uploader.id = ?1")
+    List<Video> findVideos(String id);
 }
