f2a45863194964cd6b46c5c55f0561737ca3d9c7
2hoyeong <leehodud302@naver.com>
2021-03-06 22:28:38 +0900

    feat : Video title 수정하는 Dto 추가
    Video의 title을 수정하는데 사용할 Dto를 추가

---
+++ src/main/java/me/hoyoung/youtube/web/dto/VideoTitleModifyDto.java
@@ -0,0 +1,11 @@
+package me.hoyoung.youtube.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VideoTitleModifyDto {
+    private String id;
+    private String title;
+}
