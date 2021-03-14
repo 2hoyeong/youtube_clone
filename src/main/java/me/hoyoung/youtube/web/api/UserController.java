3648803cbe209b51adb8a4c733321307f97ba5df
2hoyeong <leehodud302@naver.com>
2021-03-15 00:39:21 +0900

    refactor : RequestMapping을 이용해 중복되는 URL 제거
    RequestMapping을 이용해 중복되는 URL 매핑 제거

--- src/main/java/me/hoyoung/youtube/web/api/UserController.java
+++ src/main/java/me/hoyoung/youtube/web/api/UserController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import me.hoyoung.youtube.config.auth.JwtTokenProvider;
 import me.hoyoung.youtube.domain.user.User;
 import me.hoyoung.youtube.domain.user.VideoListResponse;
-import me.hoyoung.youtube.domain.video.Video;
 import me.hoyoung.youtube.service.UserService;
 import me.hoyoung.youtube.web.dto.UserSignInDto;
 import me.hoyoung.youtube.web.dto.UserTokenResponse;
@@ -12,30 +11,32 @@ import me.hoyoung.youtube.web.dto.UserSignUpDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserService userService;
     private final JwtTokenProvider provider;
 
-    @PostMapping("/api/v1/user/signin")
+    @PostMapping("/signin")
     public UserTokenResponse signIn(@RequestBody UserSignInDto requestDto) {
         User user = userService.signIn(requestDto);
         return new UserTokenResponse(provider.generateToken(user));
     }
 
-    @PostMapping("/api/v1/user/signup")
+    @PostMapping("/signup")
     public void signUp(@RequestBody UserSignUpDto requestDto) {
         userService.signUp(requestDto);
     }
 
 
-    @GetMapping("/api/v1/user/{userId}/videoList")
+    @GetMapping("/{userId}/videoList")
     public ResponseEntity<List<VideoListResponse>> getVideoListByUserId(@PathVariable("userId") String id) throws IOException {
         List<VideoListResponse> videoList = userService.findVideoList(id);
         return new ResponseEntity<>(videoList, HttpStatus.OK);
