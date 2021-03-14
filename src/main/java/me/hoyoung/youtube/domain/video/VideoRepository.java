44c8688cb43160c92599cbf405952d35b38e507a
2hoyeong <leehodud302@naver.com>
2021-03-14 23:42:12 +0900

    feat : 비디오 정보 조회 쿼리 선언
    id로 비디오를 조회해 원하는 정보를 가져오는 쿼리 선언

--- src/main/java/me/hoyoung/youtube/domain/video/VideoRepository.java
+++ src/main/java/me/hoyoung/youtube/domain/video/VideoRepository.java
@@ -4,6 +4,7 @@ import me.hoyoung.youtube.domain.user.VideoListResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,7 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
 
     @Query("SELECT id as id, thumbnailPath as thumbnailPath, createdDate as createdDate, title as title FROM Video ORDER BY RAND()")
     List<VideoListResponse> findRandomVideo(Pageable pageable);
+
+    @Query("SELECT v.id as id, v.thumbnailPath as thumbnailPath, v.createdDate as createdDate, v.title as title, u.name as name, u.profileImage as profileImage, v.views as views FROM Video as v JOIN User as u on u.uuid = v.uploader WHERE v.id = :id")
+    VideoResponse findVideoInfo(@Param("id") String id);
 }
