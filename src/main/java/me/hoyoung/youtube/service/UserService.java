8efd91a79b1c71cce843eeb61d335a6af6d1b142
2hoyeong <leehodud302@naver.com>
2021-03-17 03:17:04 +0900

    feat : updateProfileImage 서비스 구현
    클라이언트로 전송된 이미지를 디스크에 저장하고 사용자의 DB에 해당 이미지 파일 이름을 저장하도록 구현

--- src/main/java/me/hoyoung/youtube/service/UserService.java
+++ src/main/java/me/hoyoung/youtube/service/UserService.java
@@ -4,19 +4,25 @@ import lombok.RequiredArgsConstructor;
 import me.hoyoung.youtube.domain.user.User;
 import me.hoyoung.youtube.domain.user.UserRepository;
 import me.hoyoung.youtube.domain.user.VideoListResponse;
+import me.hoyoung.youtube.service.drive.DiskDrive;
 import me.hoyoung.youtube.web.dto.UserSignInDto;
 import me.hoyoung.youtube.web.dto.UserSignUpDto;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    private final DiskDrive profileDrive;
 
     @Transactional
     public User signIn(UserSignInDto requestDto) {
@@ -59,5 +65,13 @@ public class UserService {
         User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
         List<VideoListResponse> vlist = userRepository.findVideos(user.getUuid());
         return vlist;
+    }
+
+    @Transactional
+    public void updateProfileImage(MultipartFile File) throws IOException {
+        String filename = profileDrive.createFile(File);
+        Long userId = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUuid();
+        User user = userRepository.findById(userId).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        user.setProfileImage(filename);
     }
 }
