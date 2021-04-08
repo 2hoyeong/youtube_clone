e2fff2b5242f74ed29c87aa96ab1a753a174b96a
2hoyeong <leehodud302@naver.com>
2021-04-08 20:56:10 +0900

    feat : 댓글 리스트 API 구현
    댓글 리스트 API 구현

--- src/main/java/me/hoyoung/youtube/web/api/CommentController.java
+++ src/main/java/me/hoyoung/youtube/web/api/CommentController.java
@@ -1,9 +1,14 @@
 package me.hoyoung.youtube.web.api;
 
 import lombok.RequiredArgsConstructor;
+import me.hoyoung.youtube.domain.comment.CommentListDao;
 import me.hoyoung.youtube.service.CommentService;
 import me.hoyoung.youtube.web.dto.CommentAddDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +22,12 @@ public class CommentController {
     public void createVideo(
             @RequestBody CommentAddDto commentAddDto, @PathVariable("videoId") String videoId) {
         commentService.addComment(videoId, commentAddDto.getContent());
+    }
+
+    @GetMapping(value = "/{videoId}")
+    public ResponseEntity<List<CommentListDao>> getVideoComments(@PathVariable("videoId") String videoId) {
+        List<CommentListDao> commentList = commentService.getCommentList(videoId);
+        return new ResponseEntity(commentList, HttpStatus.OK);
     }
 
 }
