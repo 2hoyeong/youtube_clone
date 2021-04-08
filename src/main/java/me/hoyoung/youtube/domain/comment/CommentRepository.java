23ca7a1ace650fd08a5582b0c0f4fe5a8711eb41
2hoyeong <leehodud302@naver.com>
2021-04-08 20:54:36 +0900

    feat : 댓글 리스트 조회 구현
    video id로 댓글 리스트를 불러오는 쿼리 작성 및 Dao 추가

--- src/main/java/me/hoyoung/youtube/domain/comment/CommentRepository.java
+++ src/main/java/me/hoyoung/youtube/domain/comment/CommentRepository.java
@@ -1,7 +1,15 @@
 package me.hoyoung.youtube.domain.comment;
 
+import me.hoyoung.youtube.domain.video.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c.cuid as cuid, c.content as content, c.createdDate as createdDate, u.name as name FROM Comment c LEFT JOIN User u ON c.author = u.uuid WHERE c.video = :video")
+    List<CommentListDao> findAllComment(@Param("video") Video video);
 }
 
